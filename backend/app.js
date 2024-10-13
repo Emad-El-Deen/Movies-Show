71731267857a7da331b6fb6700e6aee2bb3d360e
Yehiazzz <yehiazakariaaz@gmail.com>
2024-10-13 20:23:43 +0300

    backend

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,8 @@ const authRoutes = require("./routes/authRoutes");
 
 app.use(express.json());
 app.use(cors());
-
 const mongoUrl = process.env.MONGODB_URL;
+mongoose.set("strictQuery", false);
 mongoose.connect(mongoUrl, (err) => {
   if (err) throw err;
   console.log("Mongodb connected...");
@@ -17,14 +17,14 @@ mongoose.connect(mongoUrl, (err) => {
 
 app.use("/api/auth", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../build/index.html"))
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.resolve(__dirname, "../dist")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "../dist/index.html"))
+//   );
+// }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Backend is running on port ${port}`);
 });
